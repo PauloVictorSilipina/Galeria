7a94226b137ae9185ebbed0bc5a9befe00a66c40
Paulo Victor <paulovictoralves.contato@gmail.com>
2023-04-05 12:54:33 -0300

    Passo 8, Tarefa 3, 05/04 finalizei a navegação para outra activity com retorno de resultado e a escolha e obtenção de imagem da galeria do celular

--- app/src/main/java/rodrigues/alves/lista/activity/NewItemActivity.java
+++ app/src/main/java/rodrigues/alves/lista/activity/NewItemActivity.java
@@ -2,13 +2,31 @@ package rodrigues.alves.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import rodrigues.alves.victor.galeria.R;
 
 public class NewItemActivity extends AppCompatActivity {
 
+    static int PHOTO_PICKER_REQUEST=1;
+    Uri photoSelected=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        ImageButton imgCI = findViewById(R.id.imbCI);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+            }
+        });
     }
 }
