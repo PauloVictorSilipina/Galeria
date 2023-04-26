68591aebae2dc1e7d8836f4d38d4b6b2fc905d6b
Paulo Victor <paulovictoralves.contato@gmail.com>
2023-04-26 12:56:05 -0300

    Adicionei bitmap ao myitem

--- app/src/main/java/rodrigues/alves/lista/model/MyItem.java
+++ app/src/main/java/rodrigues/alves/lista/model/MyItem.java
@@ -1,9 +1,10 @@
 package rodrigues.alves.lista.model;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 
 public class MyItem {
-    public Uri photo; //Variável pra o endereço da imagem
+    public Bitmap photo; //Variável pra o endereço da imagem
     public String title; //Variável pra o título
     public String description; //Variável para a descrição
 }
